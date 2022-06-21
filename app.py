82edbdee12ce4a2104fecb21075e6dc7f7508baa
hexbit2 <nawazedu1774@gmail.com>
2022-06-20 21:04:59 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,4 @@
+import os
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "857274386382-v3rhdkbn9lpmd98uddc5djnt9fcdrv5a.apps.googleusercontent.com")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "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")
